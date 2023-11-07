a6cfa39c202a92de88cf6815656314a755cc87e4
outman1998 <outmanmounir98@gmail.com>
2023-11-07 19:06:44 +0100

    starting code + navigation made

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import { Box } from '@mui/material';
+import Navbar from './components/Navbar';
+import Home from '../src/pages/Home';
+import ExerciseDetail from '../src/pages/ExerciseDetail';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    <Box width="400px">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/exercide/:id" element={<ExerciseDetail />} />
+      </Routes>
+      <Footer />
+    </Box>
+  );
+}
+
+export default App;
