19fe37736d2e69c028104a8476ab4a1d58f65ee3
outman1998 <outmanmounir98@gmail.com>
2023-11-09 18:35:08 +0100

    carousel made with body-parts

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -2,7 +2,10 @@ import React from 'react';
 import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
-const BodyPart = ({item, setBodyPart, BodyPart}) => {
+const BodyPart = ({item, setBodyPart, BodyPart, imageSrc}) => 
+
+{
+  console.log(item);
   return (
     <Stack
     type="button"
@@ -11,19 +14,29 @@ const BodyPart = ({item, setBodyPart, BodyPart}) => {
     className='bodyPart-card'
     sx={{
       borderTop: BodyPart === item ? '4px solid #ff2625' : '',
-      backgroundColor: '#fff',
-      borderBottomLeftRadius: '20px',
-      width: '270px',
-      height: '280px',
+      backgroundColor: 'rgba(0, 0, 0, 0.060)',
+      width: 'auto',
       cursor: 'pointer',
-      gab: '47px'
+      gab: '47px',
+      margin: '0px 10px',
+      borderRadius: "10px"
     }}
     >
       <img 
-      src={Icon} 
+      src={imageSrc} 
       alt="dumbbell"
-      style={{width: '40px', height: '40px'}}
+      style={{width: '100%', height: '250px'}}
       />
+
+      <Typography
+      fontSize="24px"
+      fontWeight="bold"
+      color="black"
+      textTransform="capitalize"
+      p="5px 0px"
+      >
+        {item}
+      </Typography>
     </Stack>
   )
 }
--- src/components/HorizontalScrollbar.js
+++ src/components/HorizontalScrollbar.js
@@ -1,42 +1,65 @@
 import React, {useContext} from 'react';
 import { Box, Typography } from '@mui/material';
 import BodyPart from './BodyPart';
-import { ScrollMenu, VisibilityContext } from 'react-horizontal-scrolling-menu';
-import RightArrowIcon from '../assets/icons/right-arrow.png';
-import LeftArrowIcon from '../assets/icons/left-arrow.png';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 
-const LeftArrow = () => {
-    const { scrollPrev } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollPrev()} className="right-arrow">
-        <img src={LeftArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
+const imageMapping = {
+    all: 'https://www.dmarge.com/wp-content/uploads/2020/01/hotelworkout.jpg',
+    back: 'https://steelsupplements.com/cdn/shop/articles/single-arm-db-row-min_1000x.jpg?v=1662648403',
+    cardio: 'https://plunkettfitness.com/wp-content/uploads/2018/11/cardio-1024x683.jpg',
+    chest: 'https://www.mensjournal.com/.image/t_share/MTk2MTM3Mjk2NTU0Njk4MjQ1/barbell-benchpress.jpg',
+    neck: 'https://www.dmoose.com/cdn/shop/articles/1_68d5f8c2-0791-4a41-86db-e7f224b142ea.jpg?v=1649931793',
+    waist: 'https://hips.hearstapps.com/hmg-prod/images/sweaty-black-athlete-doing-sit-ups-in-a-gym-royalty-free-image-1591197876.jpg?crop=1.00xw:1.00xh;0,0&resize=2048:*',
+    shoulders: 'https://www.muscleandfitness.com/wp-content/uploads/2018/03/chris-bumstead-latraise-1109.jpg?w=1300&h=731&crop=1&quality=86&strip=all',
+    upper_arms: 'https://www.mindpumpmedia.com/hubfs/Top%20Exercises%20Big%20Arms.png',
+    lower_arms: 'https://hips.hearstapps.com/hmg-prod/images/muscle-and-fitness-royalty-free-image-1622119180.jpg',
+    lower_legs: 'https://manofmany.com/wp-content/uploads/2023/09/16-Best-Calf-Exercises-and-Workouts-for-Men.jpg',
+    upper_legs: 'https://steelsupplements.com/cdn/shop/articles/shutterstock_749160127_1000x.jpg?v=1625421239'
+  };
+
+  const getImageSrc = (item) => {
+    const itemNameWithoutSpaces = item.replace(/\s/g, '_'); // Replace spaces with underscores
+    return imageMapping[itemNameWithoutSpaces] || '';
   };
   
-  const RightArrow = () => {
-    const { scrollNext } = useContext(VisibilityContext);
-  
-    return (
-      <Typography onClick={() => scrollNext()} className="left-arrow">
-        <img src={RightArrowIcon} alt="right-arrow" />
-      </Typography>
-    );
-  };
 
 export default function HorizontalScrollbar({ data, setBodyPart }) {
+
+    const responsive = {
+        0: {
+            items: 2,
+        },
+        512: {
+            items: 3
+        }
+    };
+    
   return (
-    <ScrollMenu LeftArrow={LeftArrow} RightArrow={RightArrow}>
+    <AliceCarousel
+    mouseTracking 
+    infinite
+    autoPlayInterval={1500}
+    animationDuration={1500}
+    disableDotsControls
+    responsive={responsive}
+    autoPlay
+    >
       {data.map((item) => (
         <Box
           key={item.id || item}
           itemID={item.id || item}
           title={item.id || item}
         >
-          <BodyPart BodyPart={BodyPart} item={item} setBodyPart={setBodyPart} />
+
+          <BodyPart 
+            BodyPart={BodyPart} 
+            item={item} 
+            setBodyPart={setBodyPart} 
+            imageSrc={getImageSrc(item)}
+          />
         </Box>
       ))}
-    </ScrollMenu>
+    </AliceCarousel>
   );
 }
